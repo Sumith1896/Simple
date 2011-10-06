9ecf9d9d93e07062a79062670bbb0d9f81f0a09c
Nicholas Legg <netkcid@gmail.com>
2011-10-05 23:42:56 -0400

    small player manager changes

--- Simple 2D Animation/Source/PlayerManager.h
+++ Simple 2D Animation/Source/PlayerManager.h
@@ -10,9 +10,19 @@
 #pragma once
 #include "Helpers.h"
 
+struct CACHE_ALIGN Player
+{
+	D3DXVECTOR3				d3dPosition;		//position of the player in the world
+	D3DXVECTOR3				d3dLookAt;			//direction the player is looking
+	float					fSpeedMax;			//max speed the player can move
+	float					fAcceleration;		//rate at which the player increases speed
+};
+
 class CACHE_ALIGN PlayerManager  
 {
 private:
+
+	Player		m_player;	//the player!
 
 	static	PlayerManager *m_pInstance;			//instance to the singleton	
 
@@ -24,6 +34,12 @@ public:
 
 	//	~PlayerManager: Destructor
 	~PlayerManager(){};
+
+	//////////////////////////////////////////////////////////////////////////
+	//INLINES
+	//////////////////////////////////////////////////////////////////////////
+	inline D3DXVECTOR3 GetPosition(){return m_player.d3dPosition;}
+	inline D3DXVECTOR3 GetLookAt(){return m_player.d3dLookAt;}
 
 	//////////////////////////////////////////////////////////////////////////
 	// 
@@ -43,5 +59,23 @@ public:
 	//////////////////////////////////////////////////////////////////////////
 	static PlayerManager *GetInstance();
 
-	void Init();
+	//////////////////////////////////////////////////////////////////////////
+	// 
+	//	Function: 		Init
+	//
+	//	Purpose:		Initializes the player manager
+	//
+	//////////////////////////////////////////////////////////////////////////
+	bool Init();
+
+	//////////////////////////////////////////////////////////////////////////
+	// 
+	//	Function: 		Update
+	//
+	//	Purpose:		General update for the player.
+	//					This will branch out into all the different ways a 
+	//					player can be updated.
+	//
+	//////////////////////////////////////////////////////////////////////////
+	void Update();
 };
